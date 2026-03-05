f1e11e96d169385f65fc1162d6333cbb1114509a
nguyen <abc@gmail.com>
2026-03-05 09:17:19 +0700

    Added Label and TextBox for Student Name

--- WinFormsStudentApp/WinFormsStudentApp/Form1.Designer.cs
+++ WinFormsStudentApp/WinFormsStudentApp/Form1.Designer.cs
@@ -29,12 +29,53 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtNguyenNghia = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(278, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtNguyenNghia
+            // 
+            this.txtNguyenNghia.Location = new System.Drawing.Point(374, 131);
+            this.txtNguyenNghia.Name = "txtNguyenNghia";
+            this.txtNguyenNghia.Size = new System.Drawing.Size(100, 22);
+            this.txtNguyenNghia.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtNguyenNghia);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox txtNguyenNghia;
     }
 }
 
